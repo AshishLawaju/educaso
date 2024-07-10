d3bcd204b5fe685131303883ab39941929a9b93c
AshishLawaju <ashish.lawaju74@gmail.com>
2024-07-10 14:11:31 +0545

    contact banner fix

--- src/components/contactbanner/ContactBanner.tsx
+++ src/components/contactbanner/ContactBanner.tsx
@@ -5,13 +5,13 @@ import Button from "../common/button/Button";
 const ContactBanner = () => {
   const router = useRouter()
   return (
-    <section className="container py-10x sm:py-40x lg:py-[116px]">
+    <section className="sm:container py-10x sm:py-40x lg:py-[116px]">
       <div className="flex flex-col items-center justify-between rounded-3xl bg-primary p-[32px] sm:p-[60px] text-background max-lg:justify-start lg:flex-row">
         <div>
           <h2 className="sub-heading text-background lg:w-[55%]">
             There&apos;s a University waiting to fulfil your study goals
           </h2>
-          <p className="mt-2 w-[80%] lg:mt-3 lg:w-[60%] text-span-white">
+          <p className="mt-2 sm:w-[80%] lg:mt-3 lg:w-[60%] text-span-white">
             Get expert advice on choosing education institutes, addressing the
             admissions process, scholarships, etc.
           </p>
